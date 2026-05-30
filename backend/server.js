0b0ee4abad8ba95e0e5a135387d8cee690704591
Muhammad Rehan <exaltconsultant296@gmail.com>
2026-05-30 19:03:33 +0300

    use new app error middleware

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 const connectDB = require("./config/db");
-const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
+const { notFound, errorHandler } = require("./middleware/appError");
 const listingRoutes = require("./routes/listingRoutes");
 const authRoutes = require("./authRoutes");
 const depositRoutes = require("./routes/depositRoutes");
