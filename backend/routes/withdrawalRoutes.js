729e24e0806dc08ffc117a71916b73812a3685b1
Muhammad Rehan <exaltconsultant296@gmail.com>
2026-06-02 19:08:58 +0300

    Fix user model path in withdrawal routes

--- backend/routes/withdrawalRoutes.js
+++ backend/routes/withdrawalRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Withdrawal = require("../models/Withdrawal");
-const User = require("../models/User");
+const User = require("../models/user");
 const Transaction = require("../models/Transaction");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
 
